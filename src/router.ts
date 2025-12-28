ae72e31f749d565f7905113086d095201511d4a4
NBekhruzbek <bekhruzbek2022@gmail.com>
2025-12-28 23:11:42 +0900

    fix: removed APIs from router.ts

--- src/router.ts
+++ src/router.ts
@@ -2,8 +2,4 @@ import express from "express";
 const router = express.Router();
 import memberController from "./controllers/member.controller";
 
-router.get("/", memberController.goHome);
-router.get("/signup", memberController.getSignup);
-router.get("/login", memberController.getLogin);
-
 export default router;
