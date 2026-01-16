5c81308a77ffc759609cbd8d063b96d26ba01a4a
NBekhruzbek <bekhruzbek2022@gmail.com>
2026-01-17 02:45:02 +0900

    feat: develop checkAuthSession API

--- src/controllers/admin.controller.ts
+++ src/controllers/admin.controller.ts
@@ -3,6 +3,7 @@ import { T } from "../libs/types/common";
 import MemberService from "../models/Member.service";
 import { AdminRequest, LoginInput, MemberInput } from "../libs/types/member";
 import { MemberType } from "../libs/enums/member.enum";
+import { Message } from "../libs/Errors";
 
 const memberService = new MemberService();
 
@@ -66,6 +67,20 @@ adminController.processLogin = async (req: AdminRequest, res: Response) => {
     });
   } catch (err) {
     console.log("ERROR, processLogin!", err);
+    res.send(err);
+  }
+};
+
+adminController.checkAuthSession = async (req: AdminRequest, res: Response) => {
+  try {
+    console.log("checkAuthSession");
+    if (req.session?.member) {
+      res.send(
+        `<script> alert("HI, ${req.session.member.memberNick}")</script>`
+      );
+    } else res.send(`<script> alert("${Message.NOT_AUTHENTICATED}")</script>`);
+  } catch (err) {
+    console.log("ERROR, checkAuthSession!", err);
     res.send(err);
   }
 };
