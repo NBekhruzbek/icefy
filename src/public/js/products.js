06f8955736473499399d4fcf5a8c75ba1f912182
NBekhruzbek <bekhruzbek2022@gmail.com>
2026-01-23 10:54:31 +0900

    feat: update chosen product frontend logic

--- src/public/js/products.js
+++ src/public/js/products.js
@@ -57,12 +57,35 @@ function setStatusBg(select) {
 // apply on page load
 document.querySelectorAll(".new-product-status").forEach(setStatusBg);
 
+$(".new-product-status").on("change", async function (e) {
+  const id = e.target.id;
+  const productStatus = $(`#${id}.new-product-status`).val();
+  console.log("id:", id);
+  console.log("productStatus: ", productStatus);
+
+  try {
+    const response = await axios.post(`/admin/product/${id}`, {
+      productStatus: productStatus,
+    });
+    console.log("response: ", response);
+    const result = response.data;
+    if (result) {
+      console.log("Product Updated!.");
+      $(".new-product-status").blur();
+    } else {
+      alert("Product update failed!");
+    }
+  } catch (err) {
+    console.log(err);
+    alert("Product update failed!");
+  }
+});
+
 // ---------------------------
 function validateForm() {
   const productName = $(".product-name").val();
   const productDesc = $(".product-desc").val();
   const productPrice = $(".product-price").val();
-  const discountPercent = $(".discount-percent").val();
   const productLeftCount = $(".product-left-count").val();
   const productCategory = $(".product-category").val();
   const productFlavor = $(".product-flavor").val();
@@ -72,7 +95,6 @@ function validateForm() {
     productName === "" ||
     productDesc === "" ||
     productPrice === "" ||
-    discountPercent === "" ||
     productLeftCount === "" ||
     productCategory === "" ||
     productFlavor === "" ||
